cc7836bc6a9e25a4bf5d4337ae80636e2cb2226d
Oscar-Arias2001 <oscarariasodag2000@gmail.com>
2022-11-30 21:45:33 -0600

    Agregando estructura de aplicación con Flask, vistas Acerca de y Contacto finalizadas, las demás en proceso

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, flash, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/properties")
+def properties():
+    return render_template("properties.html")
+
+@app.route("/log_in")
+def log_in():
+    return render_template("log_in.html")
+
+
+
+
+    
